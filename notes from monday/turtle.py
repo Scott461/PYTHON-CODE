2292119054878de811a95f244d6624502d35e8b1
Scott Neilson <W0422816@nscc.ca>
2019-09-16 10:13:52 -0300

    new stuff

---
+++ notes from monday/turtle.py
@@ -0,0 +1,14 @@
+print ("Hello World")
+import turtle
+
+turtle.color("red")
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(180)
+turtle.back(100)
+turtle.left(45)
+turtle.left(45)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(200)
